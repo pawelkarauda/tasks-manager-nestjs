4e8a964639beeae7e7a70ececf2fd8c9d4252844
Pawel Karauda <pawelkarauda@bitbucket.com>
2019-10-27 22:33:43 +0100

    CRUD for tasks is done

---
+++ src/tasks/dto/getTasksFilter.dto.ts
@@ -0,0 +1,13 @@
+import { TaskStatus } from '../task.model';
+import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
+import { TaskStatusValidationPipe } from '../pipes/taskStatusValidation.pipe';
+
+export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+  status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  search: string;
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/createTask.dto';
+import { GetTasksFilterDto } from './dto/getTasksFilter.dto';
+import { TaskStatusValidationPipe } from './pipes/taskStatusValidation.pipe';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+    return Object.keys(filterDto).length
+      ? this.tasksService.getTasksWithFilters(filterDto)
+      : this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
