1d78c6d1dccd6d5829c4d24fa108244d1612d801
Pawel Karauda <pawelkarauda@bitbucket.com>
2019-11-05 22:33:23 +0100

    AUth done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/authCredentials.dto';
 import { AuthService } from './auth.service';
+import { SignInCredentialsDto } from './dto/signInCredentials.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,5 +9,12 @@ export class AuthController {
   @Post('/signup')
   signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) signInCredentialsDto: SignInCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(signInCredentialsDto);
   }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -4,7 +4,11 @@ import {
   PrimaryGeneratedColumn,
   Column,
   Unique,
+  OneToMany,
 } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
+import { Task } from '../tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -17,4 +21,15 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @Column()
+  salt: string;
+
+  @OneToMany(type => Task, task => task.user, { eager: true })
+  tasks: Task[];
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
