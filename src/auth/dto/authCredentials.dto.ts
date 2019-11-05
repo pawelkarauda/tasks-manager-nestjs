6c619c0bd7828721482d463581627dd0906e08a4
Pawel Karauda <pawelkarauda@bitbucket.com>
2019-11-05 22:41:23 +0100

    Added swagger docs

--- src/auth/dto/authCredentials.dto.ts
+++ src/auth/dto/authCredentials.dto.ts
@@ -1,14 +1,17 @@
 import { MinLength, IsString, MaxLength, Matches } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class AuthCredentialsDto {
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiModelProperty()
   username: string;
 
   @IsString()
   @MinLength(6)
   @MaxLength(20)
+  @ApiModelProperty()
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'password too weak',
   })
